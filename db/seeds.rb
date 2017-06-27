09baedb8ca0e6698dd6dc6a29a287157229928cf
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 17:13:18 +0000

    Implemented custom scopes for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end 
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Melbourne Castle was an incomplete medieval castle, founded in 1311 by Thomas, 2nd Earl of Lancaster, in Melbourne, Derbyshire. It was built onthe site of an earlier royal manor house that had provided accommodation for noblemen hunting in a nearby royal park in the reign of King John. After the earl's execution in 1322 for opposing Edward II, the castle was mainly in the possession of the Crown or the Earls and Dukes of Lancaster."
+    body: "Melbourne Castle was an incomplete medieval castle, founded in 1311 by Thomas, 2nd Earl of Lancaster, in Melbourne, Derbyshire. It was built onthe site of an earlier royal manor house that had provided accommodation for noblemen hunting in a nearby royal park in the reign of King John. After the earl's execution in 1322 for opposing Edward II, the castle was mainly in the possession of the Crown or the Earls and Dukes of Lancaster.",
+   topic_id: Topic.last.id
     )
 end
 
@@ -16,10 +25,23 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400", 
+    thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
