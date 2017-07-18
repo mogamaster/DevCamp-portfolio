fe20b6438aa87f4a74344b578b24a308562d63ad
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 15:48:38 +0000

    Updated auth to include draft and published status toggle controls and implemented index styles for blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,8 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
@@ -55,7 +56,7 @@ class BlogsController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
@@ -64,6 +65,16 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
+  end
+  
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
