f5a43034f5356695f499ed626975b71437dc6a9f
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 15:07:58 +0000

    Added validations for topics in blogs and added a topic form element

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+User.create!(
+  email: "test@test.com",
+  password: "chipruby6",
+  password_confirmation: "chipruby6",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
